8364161df1cd4a49e1d301fc44dd95cb3d484881
Cuong <cuongtran73d1@gmail.com>
2021-10-23 20:03:27 +0700

    add processing data

---
+++ flower_class_extract.m
@@ -0,0 +1,20 @@
+clc
+clear all
+
+%Labeling
+class_index_move = 18
+name_class = 'lily'
+
+dataset_path = ['datasets\', name_class]
+mkdir(dataset_path)
+
+label = load('F:\archive\imagelabels.mat');
+label = label.labels;
+path = 'F:\archive\jpg\';
+files = dir([path, '\*.jpg']);
+for i = 1:length(files)
+        if label(i) == class_index_move
+                scr = [path, files(i).name];
+                copyfile(scr, dataset_path);
+        end
+end
