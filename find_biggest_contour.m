df8aff53724b20d1c88bab5d6b670aa0493770e5
DuyNguDao <87271954+DuyNguDao@users.noreply.github.com>
2021-10-28 19:51:04 +0700

    Delete find_biggest_contour.m

--- find_biggest_contour.m
+++
@@ -1,8 +0,0 @@
-function out = find_biggest_contour(edge)
-%Find biggest contour
-im_fill = imfill(edge, 'holes');
-s = regionprops(im_fill, 'Area', 'PixelList');
-[~,ind] = max([s.Area]);
-pix = sub2ind(size(edge), s(ind).PixelList(:,2), s(ind).PixelList(:,1));
-out = zeros(size(edge));
-out(pix) = edge(pix);
