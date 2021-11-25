29cd9a22ccab3e14a218d1cef256f28b12eb12c6
Cuong <cuongtran73d1@gmail.com>
2021-11-25 20:10:58 +0700

    HU problem

---
+++ extract_hu_moment_from_mask.m
@@ -0,0 +1,38 @@
+clc;
+clear all;
+addpath(genpath('.\ultis'));
+addpath(genpath('.\models'));
+
+% This module: extract Hu moment matrix and save file mat from
+% datasets_binary_mask
+% Structure: Same origin dataset but format of image is .tif (binary image)
+% Dev: Cuong Tran, Thien Le Van, Duy Ngu Dao
+
+path_datasets_binary_mask = 'datasets_seg\'
+
+name_class = {'daisy', 'rose', 'hibiscus', 'lotus', 'sunflower'};
+datasets = [];
+
+format long;
+for i = 1:5
+    dataset_path = [path_datasets_binary_mask, char(name_class(i))]    
+    files = dir([dataset_path, '\*.tif']);
+    list_S = [];
+    list_label = []
+    for j = 1:length(files)
+        image = imread([dataset_path, '\',files(j).name]);
+        image = image(:,:,1);
+        image = logical(image);
+	
+	% Feature extract
+ 	%S = hu_moment(image);
+        S = hu_moment_2(image);
+
+        label = i;
+        data = [label, S]
+        datasets(end+1, :) = data;
+    end
+end
+save hu_3 datasets
+
+
